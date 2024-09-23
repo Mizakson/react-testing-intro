cde1b84ca31b7676b6c757f576f6b7646cab4e3c
Mizakson <maxwellizakson@gmail.com>
2024-09-23 14:33:34 -0400

    Finish "Simulating user events"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 // App.jsx
 
-const App = () => <h1>First Test</h1> 
+import { useState } from "react"
+
+const App = () => {
+  const [heading, setHeading] = useState("Magnificent Monkeys")
+
+  const clickHandler = () => {
+    setHeading("Radical Rhinos")
+  }
+
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  )
+
+} 
 
 export default App
